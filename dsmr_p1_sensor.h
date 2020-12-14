e05130968e7530b556707c4eca3fd1920974019f
Steven de Jong <jongdesteven@gmail.com>
2020-12-14 11:02:31 +0100

    Changed GPIO and sensors for my environment

--- dsmr_p1_sensor.h
+++ dsmr_p1_sensor.h
@@ -4,7 +4,7 @@
 using namespace esphome;
 
 #define P1_MAXTELEGRAMLENGTH 1500
-#define DELAY_MS 60000 // Delay in miliseconds before reading another telegram
+#define DELAY_MS 5000 // Delay in miliseconds before reading another telegram
 #define WAIT_FOR_DATA_MS 2000
 
 // Use data structure according to: https://github.com/matthijskooijman/arduino-dsmr
@@ -28,6 +28,10 @@ using MyData = ParsedData <
   /* FixedValue */ power_returned_l1,
   /* FixedValue */ power_returned_l2,
   /* FixedValue */ power_returned_l3,
+  /* uint16_t */ water_device_type,
+  /* String */ water_equipment_id,
+  /* uint8_t */ water_valve_position,
+  /* TimestampedFixedValue */ water_delivered, 
   /* uint16_t */ gas_device_type,
   /* uint8_t */ gas_valve_position,
   /* TimestampedFixedValue */ gas_delivered
@@ -134,6 +138,9 @@ class CustomP1UartComponent : public Component, public uart::UARTDevice {
 	if(data.gas_device_type_present)s_gas_device_type->publish_state(data.gas_device_type);
 	if(data.gas_valve_position_present)s_gas_valve_position->publish_state(data.gas_valve_position);
 	if(data.gas_delivered_present)s_gas_delivered->publish_state(data.gas_delivered);
+	if(data.water_device_type_present)s_water_device_type->publish_state(data.water_device_type);
+	if(data.water_valve_position_present)s_water_valve_position->publish_state(data.water_valve_position);
+	if(data.water_delivered_present)s_water_delivered->publish_state(data.water_delivered);
   };  
    
  public:
@@ -161,11 +168,14 @@ class CustomP1UartComponent : public Component, public uart::UARTDevice {
   Sensor *s_gas_device_type = new Sensor();
   Sensor *s_gas_valve_position = new Sensor();
   Sensor *s_gas_delivered = new Sensor(); 
+  Sensor *s_water_device_type = new Sensor();
+  Sensor *s_water_valve_position = new Sensor();
+  Sensor *s_water_delivered = new Sensor(); 
 
   void setup() override {
     lastread = 0;
-	pinMode(D5, OUTPUT); // Set D5 as output pin
-	digitalWrite(D5,LOW); // Set low, don't request message from P1 port
+	pinMode(4, OUTPUT); // Set D5 as output pin
+	digitalWrite(4,LOW); // Set low, don't request message from P1 port
   }
   
   void loop() override {
@@ -173,11 +183,11 @@ class CustomP1UartComponent : public Component, public uart::UARTDevice {
 	
 	if (now - lastread > DELAY_MS || lastread == 0) {
 		lastread = now;
-		digitalWrite(D5,HIGH); // Set high, request new message from P1 port
+		digitalWrite(4,HIGH); // Set high, request new message from P1 port
 		if (data_available()) { // Check for x seconds if there's data available
 			bool have_message = read_message();
 			if (have_message) { 
-				digitalWrite(D5,LOW); // Set low, stop requesting messages from P1 port
+				digitalWrite(4,LOW); // Set low, stop requesting messages from P1 port
 			} // If No message was read, keep output port high and retry later
 		} else {
 				ESP_LOGD("DmsrCustom","No data available. Is P1 port connected?");
